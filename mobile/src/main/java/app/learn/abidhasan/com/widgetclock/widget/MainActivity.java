7e2a546e49a47b1bbf7a28958f5fc11a2ef61980
Abid Hasan <la4508@gmail.com>
2017-07-09 22:35:16 +0600

    step one design widget and make backward compatible

---
+++ mobile/src/main/java/app/learn/abidhasan/com/widgetclock/widget/MainActivity.java
@@ -0,0 +1,13 @@
+package app.learn.abidhasan.com.widgetclock.widget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import app.learn.abidhasan.com.widgetclock.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
